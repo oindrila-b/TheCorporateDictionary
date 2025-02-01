2a79f84969e2c6cc1613c53cccbc01f3e7d4bcb5
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-02-02 00:09:53 +0530

    feat(changes): Added new code

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Livvic, Reddit_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const redditMono = Reddit_Mono({
+  weight: "300",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const livvicMed = Livvic({
+  weight: "500",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${livvicMed} ${redditMono} antialiased`}
       >
         {children}
       </body>
