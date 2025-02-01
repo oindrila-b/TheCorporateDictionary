87a8bc6e7388617cda9cbba7807c3d6e802a6cb2
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-02-01 21:58:12 +0530

    feat(restructure): Changed structure of the code

--- server/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-export const __esModule = true;
-import * as pino from 'pino';
-var logger = pino["default"]();
-logger.info('hello world');
-var child = logger.child({ a: 'property' });
-child.info('hello child!');
--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as pino from 'pino'
-const logger = pino.default()
-
-logger.info('hello world')
-
-const child = logger.child({ a: 'property' })
-child.info('hello child!')
