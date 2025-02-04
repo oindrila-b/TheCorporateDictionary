e86c8db30567974a032233cd53fe63f91348e01b
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-02-04 23:54:54 +0530

    ADD: Added more code changes, component introduction

---
+++ client/src/app/components/homepage/homepage.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Homepage() {
+  return (
+    <div className="">
+    <Image src="/images/tcd_logo.png" alt="" width={150} height={150}/>
+  </div>
+
+  );
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
+import Homepage from "./components/homepage/homepage";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-4 sm:p-16 tracking-widest overflow-auto font-[family-name:livvic]">
-      <div className="overflow-auto">
-        <Image src="/images/tcd_logo.png" alt="" width={150} height={150}/>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-4 sm:p-16 tracking-widest overflow-auto font-[family-name:livvic] overflow-auto">
+      <div>
+        <Homepage/>
       </div>
-
     </div>
   );
 }
