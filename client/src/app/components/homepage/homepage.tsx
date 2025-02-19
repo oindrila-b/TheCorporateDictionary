567ae8bc906f0ae3a24eda4da9d41aed0888800b
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-02-20 01:55:15 +0530

    changes

--- client/src/app/components/homepage/homepage.tsx
+++ client/src/app/components/homepage/homepage.tsx
@@ -4,6 +4,7 @@ export default function Homepage() {
   return (
     <div className="">
     <Image src="/images/tcd_logo.png" alt="" width={150} height={150}/>
+    <p> This is the homepage</p>
   </div>
 
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-4 sm:p-16 tracking-widest overflow-auto font-[family-name:livvic] overflow-auto">
       <div>
         <Homepage/>
+
       </div>
     </div>
   );
