23daaa28209e6647d32e1949c08da359436a1fdb
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-02-24 12:30:25 +0530

    ADD: Modified Homepage

--- client/src/app/components/homepage/homepage.tsx
+++ client/src/app/components/homepage/homepage.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Homepage() {
   return (
-    <div className="">
+    <div className="text-xl md:text-5xl p-4 mt-16">
     <Image src="/images/tcd_logo.png" alt="" width={150} height={150}/>
-    <p> This is the homepage</p>
+       <div className="text-xl md:text-5xl p-4 m-16">
+       <h1> Find any Corporate Term!</h1>
+       </div>
   </div>
 
   );
